5a0a27fc71c5e8e52551e4d89c6c9f18fe7e188b
MKuznetsov <kuznetsomaks@gmail.com>
2023-11-21 23:31:03 +0200

    [ENG-009] Implement “Add/Remove Elements” scenarios [ENG-010] Implement “Basic Auth” scenarios

---
+++ specs/basicAuth.spec.ts
@@ -0,0 +1,42 @@
+import {expect, test} from '@playwright/test';
+import {allure} from "allure-playwright";
+import {BasePage} from "../framework/pages/base.page";
+import {CommonHelper} from "../framework/helpers/common.helper";
+import {BasicAuthPage} from "../framework/pages/basic.auth.page";
+
+test.describe('Available Examples - Basic Auth', () => {
+
+    test.afterEach(async () => {
+        await allure.epic('The Internet');
+        await allure.feature('Available Examples');
+        await allure.story('Basic Auth');
+        await allure.suite('Basic Auth scenarios');
+        await allure.description("Check 'Basic Auth' page functionality");
+        await allure.tags('auth', 'ui');
+    });
+
+    test('Basic Auth', async ({browser}) => {
+        // Setting HTTP authentication for page
+        const page = await CommonHelper.setHttpAuth(browser, {username: 'admin', password: 'admin'});
+
+        const basePage = new BasePage(page);
+
+        // Opening main page
+        await basePage.openPage({page: "main"});
+
+        // Validating main page elements
+        await basePage.validatePage();
+
+        // Clicking on section by name
+        await basePage.clickOnSectionByName('Basic Auth');
+
+        const basicAuthPage = new BasicAuthPage(page);
+
+        // Validating "Basic Auth" page elements
+        await basicAuthPage.validatePage();
+
+        // Asserting page description
+        await expect(basicAuthPage.descriptionElement).toHaveText(basicAuthPage.descriptionText);
+    });
+
+});
