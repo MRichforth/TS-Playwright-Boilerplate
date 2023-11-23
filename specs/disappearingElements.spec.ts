e74d8c2dbaabf50cce42cd50962a61973119c77d
MKuznetsov <kuznetsomaks@gmail.com>
2023-11-23 17:17:57 +0200

    [ENG-017] Implement “Disappearing Elements” scenario

---
+++ specs/disappearingElements.spec.ts
@@ -0,0 +1,40 @@
+import {test} from '@playwright/test';
+import {allure} from "allure-playwright";
+import {BasePage} from "../framework/pages/base.page";
+import {DisappearingElementsPage} from "../framework/pages/disappearing.elements.page";
+
+test.describe('Available Examples - Disappearing Elements', () => {
+
+    let basePage: BasePage;
+
+    test.beforeEach(async ({page}) => {
+        basePage = new BasePage(page);
+        await basePage.openPage({page: "main"});
+        await basePage.validatePage();
+        await basePage.clickOnSectionByName('Disappearing Elements');
+    });
+
+    test.afterEach(async () => {
+        await allure.epic('The Internet');
+        await allure.feature('Available Examples');
+        await allure.story('Disappearing Elements');
+        await allure.suite('Disappearing Elements scenarios');
+        await allure.description("Check 'Disappearing Elements' page functionality");
+        await allure.tags('elements', 'ui');
+    });
+
+    test('Disappearing Elements ', async ({page}) => {
+        const disappearingElementsPage = new DisappearingElementsPage(page);
+
+        // Validating main page elements
+        await disappearingElementsPage.validatePage();
+
+        // Validating main page buttons
+        await disappearingElementsPage.validateButtonVisibility('Home');
+        await disappearingElementsPage.validateButtonVisibility('About');
+        await disappearingElementsPage.validateButtonVisibility('Contact Us');
+        await disappearingElementsPage.validateButtonVisibility('Portfolio');
+        await disappearingElementsPage.validateButtonVisibility('Gallery');
+    });
+
+});
