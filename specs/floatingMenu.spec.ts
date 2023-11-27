66f8d6b191761a30e9ffdd9458573cc388cf81f9
MKuznetsov <kuznetsomaks@gmail.com>
2023-11-27 20:18:04 +0200

    [ENG-027] Implement “Floating Menu” scenario

---
+++ specs/floatingMenu.spec.ts
@@ -0,0 +1,65 @@
+import {test} from '@playwright/test';
+import {allure} from "allure-playwright";
+import {BasePage} from "../framework/pages/base.page";
+import {FloatingMenuPage} from "../framework/pages/floating.menu.page";
+import {CommonAssertionsHelper} from "../framework/helpers/common.assertions.helper";
+
+test.describe('Available Examples - Floating Menu', () => {
+
+    let basePage: BasePage;
+
+    test.beforeEach(async ({page}) => {
+        basePage = new BasePage(page);
+        await basePage.openPage({page: "main"});
+        await basePage.validatePage();
+        await basePage.clickOnSectionByName('Floating Menu');
+    });
+
+    test.afterEach(async () => {
+        await allure.epic('The Internet');
+        await allure.feature('Available Examples');
+        await allure.story('Floating Menu');
+        await allure.suite('Floating Menu scenarios');
+        await allure.description("Check 'Floating Menu' page functionality");
+        await allure.tags('floating', 'ui');
+    });
+
+    test('Floating Menu', async ({page}) => {
+        const floatingMenuPage = new FloatingMenuPage(page);
+
+        // Validating main page elements
+        await floatingMenuPage.validatePage({exclude: 'description'});
+
+        // Checking if floating menu is displayed
+        await floatingMenuPage.checkVisibilityOfFloatingMenu();
+
+        // Validating "Home" button functionality
+        await floatingMenuPage.clickOnFloatMenuItem('Home');
+        await CommonAssertionsHelper.assertUrl(page, /#home/)
+
+        // Validating "News" button functionality
+        await floatingMenuPage.clickOnFloatMenuItem('News');
+        await CommonAssertionsHelper.assertUrl(page, /#news/)
+
+        // Validating "Contact" button functionality
+        await floatingMenuPage.clickOnFloatMenuItem('Contact');
+        await CommonAssertionsHelper.assertUrl(page, /#contact/)
+
+        // Validating "About" button functionality
+        await floatingMenuPage.clickOnFloatMenuItem('About');
+        await CommonAssertionsHelper.assertUrl(page, /#about/)
+
+        // Checking visibility if first paragraph on the page
+        await floatingMenuPage.checkVisibilityOfParagraph('first');
+
+        // Scrolling page to the bottom
+        await floatingMenuPage.scrollPageToBottom();
+
+        // Checking visibility of the last paragraph on the page
+        await floatingMenuPage.checkVisibilityOfParagraph('last');
+
+        // Checking if floating menu is displayed
+        await floatingMenuPage.checkVisibilityOfFloatingMenu();
+    });
+
+});
