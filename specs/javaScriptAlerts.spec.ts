99d59f2b858ec7d40b1b617386a1591eabf6dd2b
MKuznetsov <kuznetsomaks@gmail.com>
2023-12-05 11:05:46 +0200

    test

---
+++ specs/javaScriptAlerts.spec.ts
@@ -0,0 +1,83 @@
+import {test} from '@playwright/test';
+import {allure} from "allure-playwright";
+import {BasePage} from "../framework/pages/base.page";
+import {JavaScriptAlertsPage} from "../framework/pages/javascript.alerts.page";
+
+test.describe('Available Examples - JavaScript Alerts', () => {
+
+    let basePage: BasePage;
+
+    test.beforeEach(async ({page}) => {
+        basePage = new BasePage(page);
+        await basePage.openPage({page: "main"});
+        await basePage.validatePage();
+        await basePage.clickOnSectionByName('JavaScript Alerts');
+    });
+
+    test.afterEach(async () => {
+        await allure.epic('The Internet');
+        await allure.feature('Available Examples');
+        await allure.story('JavaScript Alerts');
+        await allure.suite('JavaScript Alerts scenarios');
+        await allure.description("Check 'JavaScript Alerts' page functionality");
+        await allure.tags('alerts', 'ui');
+    });
+
+    test('JavaScript Alerts - Alert', async ({page}) => {
+        const javaScriptAlertsPage = new JavaScriptAlertsPage(page);
+
+        // Validating main page elements
+        await javaScriptAlertsPage.validatePage();
+
+        // Asserting "Alert" dialog
+        await javaScriptAlertsPage.handleAlertDialog();
+        await javaScriptAlertsPage.clickOnDialogButton('Alert');
+
+        // Asserting specific dialog result
+        await javaScriptAlertsPage.assertDialogResult('Alert');
+    });
+
+    test('JavaScript Alerts - Confirm', async ({page}) => {
+        const javaScriptAlertsPage = new JavaScriptAlertsPage(page);
+
+        // Validating main page elements
+        await javaScriptAlertsPage.validatePage();
+
+        // Asserting "Alert" dialog
+        await javaScriptAlertsPage.handleConfirmDialog('accept');
+        await javaScriptAlertsPage.clickOnDialogButton('Confirm');
+
+        // Asserting specific dialog result
+        await javaScriptAlertsPage.assertDialogResult('Confirm');
+    });
+
+    test('JavaScript Alerts - Cancel', async ({page}) => {
+        const javaScriptAlertsPage = new JavaScriptAlertsPage(page);
+
+        // Validating main page elements
+        await javaScriptAlertsPage.validatePage();
+
+        // Asserting "Alert" dialog
+        await javaScriptAlertsPage.handleConfirmDialog('decline');
+        await javaScriptAlertsPage.clickOnDialogButton('Confirm');
+
+        // Asserting specific dialog result
+        await javaScriptAlertsPage.assertDialogResult('Cancel');
+    });
+
+    test('JavaScript Alerts - Prompt', async ({page}) => {
+        const javaScriptAlertsPage = new JavaScriptAlertsPage(page);
+
+        // Validating main page elements
+        await javaScriptAlertsPage.validatePage();
+
+        // Asserting "Alert" dialog
+        const promptMessage = 'TestValue';
+        await javaScriptAlertsPage.handlePromptDialog(promptMessage);
+        await javaScriptAlertsPage.clickOnDialogButton('Prompt');
+
+        // Asserting specific dialog result
+        await javaScriptAlertsPage.assertDialogResult('Prompt', promptMessage);
+    });
+
+});
